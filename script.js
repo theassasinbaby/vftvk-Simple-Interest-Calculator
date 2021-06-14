025539bbb3a913e34e1e16f435f6c03d39d9199a
theassasinbaby <aniketgpt335@gmail.com>
2021-06-14 19:28:19 +0000

    This is a Simple interest Calculator.

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var year = document.getElementById("years").value;
+    var interest = principal * year * rate /100;
+
+    var years = new Date().getFullYear()+parseInt(year);
+
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+    "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+    "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+    "in the year <mark>" + years + "</mark>";
+}
+
+function updateRate(){
+
+    var rateval= document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText= rateval;
     
+}
+
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
 }
         
